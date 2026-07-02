bc7b6423594957ed49611d18ca9a14387da38e96
Dipika Rijal <dipikarijal149@gmail.com>
2026-07-02 20:40:53 +0545

    Add AI notes quiz upload and quiz controls

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,9 @@ export default function App() {
 
       <StartModal
         modalType={modalType}
+        user={user}
         closeModal={() => setModalType(null)}
+        openAuthModal={setAuthMode}
       />
 
       <AuthModal
